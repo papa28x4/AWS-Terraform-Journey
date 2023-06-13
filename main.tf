bfba78e9348bdae291452ab8491e4c44dd99aaa4
papa28x4 <swissfair@yahoo.co.uk>
2023-06-13 15:28:14 +0100

    create vpc and public subnet

--- main.tf
+++ main.tf
@@ -12,59 +12,48 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-resource "aws_instance" "ec2_example" {
+# Create a VPC
+resource "aws_vpc" "my_company" {
+  cidr_block = "10.0.0.0/16"
 
-    ami = "ami-0eb260c4d5475b901"  
-    instance_type = "t2.micro" 
-    key_name= "aws_key_one"
-    vpc_security_group_ids = [aws_security_group.main.id]
-
-  provisioner "remote-exec" {
-    inline = [
-      "touch hello.txt",
-      "echo helloworld remote provisioner >> hello.txt",
-    ]
+  tags = {
+    Name = "my-company"
   }
-  connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ubuntu"
-      private_key = file("./aws_key_one") //This is here because we want to auto-login and do some post installation config
-      timeout     = "4m"
-   }
 }
 
-resource "aws_security_group" "main" {
-  egress = [
-    {
-      cidr_blocks      = [ "0.0.0.0/0", ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
- ingress                = [
-   {
-     cidr_blocks      = [ "0.0.0.0/0", ]
-     description      = ""
-     from_port        = 22
-     ipv6_cidr_blocks = []
-     prefix_list_ids  = []
-     protocol         = "tcp"
-     security_groups  = []
-     self             = false
-     to_port          = 22
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.app_vpc.id
+
+#   tags = {
+#     Name = "vpc_igw"
+#   }
+# }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_company.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "public-subnet"
   }
-  ]
 }
 
+# resource "aws_route_table" "public_rt" {
+#   vpc_id = aws_vpc.app_vpc.id
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "aws_key_one"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRLJoZFwpdN/4Ihc/uiYbdi2DbcIJOdO45QwMBmg/V4OQlTiQugAotnXBOh03KXUaGi+4o5v/B/qq8syCJkoEOO+yAzHp3tWAF0szLA63GABgmACiN6ZUlN0yKObIZxSvybniohPeFa5+iZHGp+Mo7n98bqBLfmUrbP8kFCD48XJ7eX6F2E3aY4KeN5LLfj6Gh34OVvG6K2N6hkW1D9jmCl5mhIfmwkVE1ou+yGsEtZoVzmGVfqwL3ARoFcUuSrwK7syiUgL8Q6kPyli8GEeXIO8rC73wyDyrzraKoNkdHJC1tFybjNjkm7rJSe+fqNzresXoOSW+tkV0GpXUNzrD3 USER@PAPA2"
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
+
+#   tags = {
+#     Name = "public_rt"
+#   }
+# }
+
+# resource "aws_route_table_association" "public_rt_asso" {
+#   subnet_id      = aws_subnet.public_subnet.id
+#   route_table_id = aws_route_table.public_rt.id
+# }
--- steps.md
+++ steps.md
@@ -1,1 +1,4 @@
-ssh-keygen -t rsa -b 2048 
+map_public_ip_on_launch - (Optional) Specify true to indicate that instances launched into the subnet should be assigned a public IP address. Default is false.
+
+aws ec2 describe-availability-zones --region region-name
+aws ec2 describe-availability-zones --region eu-west-2 (To check for availability zones: eu-west-2a, eu-west-2b, eu-west-2c)
